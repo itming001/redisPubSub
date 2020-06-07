b20455236fe8059c257a56e48387e1a29096ea26
liyanming <13315100614@163.com>
2020-06-07 14:01:25 +0800

    增加student文件实体类

---
+++ src/main/java/com/li/redispubsub/entity/Student.java
@@ -0,0 +1,15 @@
+package com.li.redispubsub.entity;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 年龄
+     */
+    private Integer age;
+}
